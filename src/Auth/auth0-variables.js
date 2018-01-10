551bdcc0a1ad992f7c753f76862a2d1a1426d0e7
duc.lt <duc.lt@sutrixsolutions.com>
2018-01-10 14:38:48 +0700

    add source code

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'duclt.auth0.com',
+  clientId: 'qs1dI0SPg2NH7U2g5MgdDmVvjbsW50l5',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'http://localhost:3000'
+}
